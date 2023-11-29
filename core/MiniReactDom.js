f9252f3d6c1bd6edbb7640ae1f9919845045580d
Christopher Debray <christopherdebray1@gmail.com>
2023-11-29 23:18:11 +0100

    feat: reset to original state

--- core/MiniReactDom.js
+++ core/MiniReactDom.js
@@ -1,6 +1,5 @@
 import BrowserRouter from "../components/BrowserRouter.js";
 import { EVENT_TYPE_LIST } from "./constants.js";
-import { cache } from "./CachingService.js";
 
 const MiniReactDom = {
   render: function (rootElement, routes) {
