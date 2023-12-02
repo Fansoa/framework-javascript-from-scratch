a7c45820f24f5d47c7da6da6aa9fe19fbd02b93e
Christopher Debray <christopherdebray1@gmail.com>
2023-12-02 14:24:38 +0100

    feat: add separate methods for props / events handling with reset parameter

--- restart/core/MiniReact.js
+++ restart/core/MiniReact.js
@@ -49,32 +49,35 @@ export class MiniReact {
       document.createTextNode(structure.props.content) :
       document.createElement(structure.type);
 
-    if (structure.props) {
-      this.setDomProps(dom, structure.props);
-      this.setDomEvents(dom, structure.props);
-    }
+    this.setDomParameters(dom, structure);
+    this.setDomChildren(dom, structure);
 
-    structure.children.forEach((childStructure) => {
-      const childDom = this.createDom(childStructure);
-      dom.appendChild(childDom);
-    });
     return dom;
   }
 
-  setDomProps(dom, props) {
+  // PROPS
+  setDomParameters(dom, structure) {
+    this.setDomProps(dom, structure.props);
+    this.setDomEvents(dom, structure.props);
+  }
+
+  // Si mustReset = true dans ce cas cela veut dire qu'on veut reset nos props (dans le cas de l'update du dom par exemple)
+  setDomProps(dom, props, mustReset = false) {
     Object.keys(props)
       .filter((propName) => !this.isEvent(propName))
       .forEach(propName => {
-        dom[propName] = props[propName]
+        dom[propName] = mustReset ? "" : props[propName];
       });
   }
 
-  setDomEvents(dom, props) {
+  // Si mustReset = true dans ce cas cela veut dire qu'on veut reset nos events (dans le cas de l'update du dom par exemple)
+  setDomEvents(dom, props, mustReset = false) {
     Object.keys(props)
     .filter((propName) => this.isEvent(propName))
     .forEach(eventName => {
       const eventType = eventName.split('.')[1]
-      dom.addEventListener(eventType, props[eventName]);
+      const eventAction = mustReset ? 'removeEventListener' : 'addEventListener';
+      dom[eventAction](eventType, props[eventName]);
     });
   }
 
@@ -82,6 +85,15 @@ export class MiniReact {
     return propName.includes('event.');
   }
 
+  // CHILDREN
+  setDomChildren(dom, structure) {
+    structure.children.forEach((childStructure) => {
+      const childDom = this.createDom(childStructure);
+      dom.appendChild(childDom);
+    });
+  }
+
+  // STATE
   useState(initialState) {
     let state = initialState;
 
