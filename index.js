66b1d8d58fb35e6caa47b34f254cfc8bac849e2c
Christopher Debray <christopherdebray1@gmail.com>
2023-12-04 17:54:10 +0100

    feat: Add functional workQueue (no child / state handling)

--- index.js
+++ index.js
@@ -1,70 +1,83 @@
-/*
-import { MiniReactInstance } from "./restart/core/MiniReact.js";
-
-function buttonComponent() {
-  const [state, setState] = MiniReactInstance.useState(0);
-  return MiniReactInstance.createElement(
-    'button',
-    {
-      'className': 'w-full bg-green-500',
-      'event.click': function (event) {
-        event.stopPropagation()
-        setState(state => state+1)
-      },
-    },
-    [
-      `Button component ${state}`
-    ]
-  );
-}
-
-function page() {
-  const [state, setState] = MiniReactInstance.useState(9);
-  return MiniReactInstance.createElement(
-    'div',
-    {
-      'id': 'myDiv',
-      'className': 'w-[150px] h-[150px] bg-red-500',
-    },
-    [
-      `Hello ${state}`,
-      MiniReactInstance.createElement(
-        'button',
-        {
-          'id': 'myButton',
-          'className': 'w-full bg-blue-500',
-          'event.click': function (event) {
-            setState(state => state+1)
-            alert(`State equal = ${state}`)
-          },
-        },
-        [
-          'Button'
-        ]
-      ),
-      buttonComponent()
-    ]
-  );
-}
-const root = document.getElementById("root");
-MiniReactInstance.render(page(), root);
-*/
-
 import { MiniReact } from "./restart_new/core/MiniReact.js";
 
 function Test() {
+  const [state, setState] = MiniReact.useState(0);
+
   return MiniReact.createElement(
     'div',
     {
       'id': 'myDiv',
       'className': 'w-[150px] h-[150px] bg-red-500',
-      'event.click': () => alert('Test'),
     },
-    ['This is a test component']
+    [
+      MiniReact.createElement(
+        'div',
+        {
+          'className': 'bg-blue-500',
+          'event.click': () => alert('Test'),
+        },
+        [
+          MiniReact.createElement(
+            'button',
+            {
+              'className': 'bg-green-500',
+              'event.click': (event) => {
+                event.stopPropagation();
+                alert('First button')
+              },
+            },
+            ['First button']
+          )
+        ]
+      ),
+      'Hello',
+      MiniReact.createElement(
+        'div',
+        {
+          'className': 'bg-pink-500',
+        },
+        [
+          MiniReact.createElement(
+            'button',
+            {
+              'className': 'bg-yellow-500',
+              'event.click': (event) => {
+                event.stopPropagation();
+                alert('MY BUTTON')
+              },
+            },
+            ['MY BUTTON']
+          ),
+          MiniReact.createElement(
+            'span',
+            {
+              'className': 'bg-orange-500',
+            },
+            [
+              MiniReact.createElement(
+                'div',
+                {
+                  'className': 'bg-green-300',
+                  'event.click': () => setState(state => state+1)
+                },
+                [`test set State = ${state}`]
+              ),
+              MiniReact.createElement(
+                'div',
+                {
+                  'className': 'bg-orange-500',
+                },
+                ['IM BLUE DADOUDI DADOUDA DADOU DI DADOU DAAAAA']
+              )
+            ]
+          )
+        ]
+      )
+    ]
   )
 }
 
-const elementTest = MiniReact.createDom(Test());
 const root = document.getElementById("root");
 
-root.appendChild(elementTest);
+const element = Test();
+MiniReact.render(element, root)
--- restart_new/core/MiniReact.js
+++ restart_new/core/MiniReact.js
@@ -1,3 +1,21 @@
+let nextTask = null;
+let renderedRoot = null;
+let currentRoot = null;
+
+// RENDER - (render the element inside the container)
+function render(element, container) {
+  // Permet de stocker l'arborescence qui est en cours
+  currentRoot = {
+    parent: container,
+    type: 'create',
+    content: element
+  }
+
+  // On utilise la workQueue donc on doit set le nextTask
+  nextTask = currentRoot;
+}
+
+// CREATE ELELEMNT - (create element structure)
 function createElement(type, props, children) {
   return {
     type,
@@ -22,17 +40,18 @@ function createTextElement(content) {
   }
 }
 
+// CREATE DOM - (create the dom of ONE element. Not in charge of child dom creation, with unitOfWork tasks they are all handled independently)
 function createDom(structure) {
-  console.warn(structure);
   const dom = structure.type === "TEXT_NODE" ?
     document.createTextNode(structure.props.content) :
-    document.createElement(structure.type);
-
+    document.createElement(structure.type);    
+  
   updateDom(dom, {}, structure);
-
+  
   return dom;
 }
 
+// UPDATE DOM - (update the dom of an element, it's props event etc. On createDom it will only be adding, not updating)
 function updateDom(dom, prevStructure, newStructure) {
   if (Object.keys(prevStructure).length) {
     setDomParameters(dom, newStructure, true);
@@ -41,6 +60,7 @@ function updateDom(dom, prevStructure, newStructure) {
   setDomParameters(dom, newStructure);
 }
 
+// SET DOM PARAMETERS & SET DOM PROPS - (in charge of setting or resetting the dom parameters, props, events etc)
 function setDomParameters(dom, structure, mustReset = false) {
   setDomProps(dom, structure.props, mustReset);
   setDomEvents(dom, structure.props, mustReset);
@@ -68,7 +88,97 @@ function isEvent(propName) {
   return propName.includes('event.');
 }
 
+// WORK QUEUE - (The functions that will allow to handle our work queue)
+function workQueue(reqIdleCall) {
+  while (nextTask) {
+    nextTask = executeTask(nextTask);
+  }
+
+  /**
+   * Si plus de nextTask ça veut dire que notre render est complété
+   * Comme ça va tourner en background (et en boucle) on check si currentRoot est présent
+   *    pour pas lancer notre commitRoot en boucle (commitRoot set currentRoot à null)
+   */
+  if (!nextTask && currentRoot) {
+    commitRoot();
+  }
+
+  requestIdleCallback(workQueue);
+}
+
+function executeTask(task) {
+  if (task.type === "create") {
+    const elementDom = createDom(task.content);
+    task.parent.appendChild(elementDom);
+
+    const child = task.content.children[0];
+
+    const nextFiber = child ? {
+      parent: elementDom,
+      parentFiber: task,
+      type: 'create',
+      content: child,
+    } : null;
+
+    let previousSibling = null;
+    // Permet de gérer les siblings, on récupère notre enfant et créer une liste chainée de sibling.
+    for (let index = 1; index < task.content.children.length; index++) {
+      const sibling = task.content.children[index];
+      const siblingFiber = sibling ? {
+        parent: elementDom,
+        parentFiber: task,
+        type: 'create',
+        content: sibling,
+      } : null;
+
+      nextFiber.sibling = siblingFiber;
+      previousSibling = siblingFiber;
+    }
+
+    task.child = nextFiber;
+
+    /**
+     * Si il y a un enfant on le renvoie afin d'être sûr de le traiter lui puis ces enfants etc.
+     * Les siblings ne seront géré qu'a la fin, lorsque l'on atteint le bout d'une branche
+     * Ensuite on remontera au parent, on executera son sibling et ainsi de suite
+     */ 
+    if (task.child) {
+      return task.child;
+    }
+
+    while (nextTask) {
+      if(nextTask && nextTask.sibling) {
+        return nextTask.sibling;
+      }
+      // Si il n'y a plus d'enfant (et ici de sibling) on retourne au parent afin de prendre en compte les sibling du parent puis ces enfants etc
+      nextTask = nextTask.parentFiber;
+    }
+  }
+}
+
+// COMMIT - (Will allow us to commit our work, setting the renderedRoot / currentRoot in order to have a sort of caching system used in case of updates)
+function commitRoot() {
+  renderedRoot = currentRoot;
+  currentRoot = null;
+}
+
+// STATE - (our state handling hook)
+function useState(initialState) {
+  let state = initialState;
+
+  const setState = callback => {
+    state = callback(state);
+    console.log(state);
+  }
+  return [state, setState];
+}
+
+// REQUEST IDLE CALLBACK - (used to initiate the start of our work queue)
+requestIdleCallback(workQueue);
+
 export const MiniReact = {
   createElement,
-  createDom
+  createDom,
+  render,
+  useState
 }
