20d8f9e8b5d979543a9ac2dd8f131db52d7e90d7
fansoa <fverin.contactpro@gmail.com>
2023-12-05 13:08:05 +0100

    reset code

--- index.js
+++
@@ -1,4 +0,0 @@
-import MiniReactDom from "./core/MiniReactDom.js";
-import routes from "./routes.js";
-
-MiniReactDom.render(document.getElementById("root"), routes);
--- routes.js
+++
@@ -1,7 +0,0 @@
-import Homepage from "./pages/Homepage.js";
-import EventListingPage from "./pages/EventListingPage.js";
-
-export default {
-  "/": new Homepage().render(),
-  "/listing-events": new EventListingPage().render(),
-};
