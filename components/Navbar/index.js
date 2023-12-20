1fa5552ae4a31530234c2f3a266c7996c6e27374
fansoa <fverin.contactpro@gmail.com>
2023-12-20 16:19:46 +0100

    feat: add NavBar

---
+++ components/Navbar/index.js
@@ -0,0 +1,33 @@
+import MiniReact from "../../core/MiniReact.js";
+import BrowserLink from "../../src/components/BrowserLink.js";
+
+class Navbar extends MiniReact.Component {
+  renderComponent() {
+    const browserLink = new BrowserLink({
+      to: "/",
+      content: `<div class='group h-full w-fit flex flex-col'>
+    <div class='flex px-2 gap-1 flex items-center h-20'>
+      <p class='text-indigo-400'>Carte Interactive</p>
+      <img src="../../assets/images/icons/map.svg">
+    </div>
+    <div class='bg-slate-300 group-hover:bg-indigo-300 h-0.5'></div>
+  </div>`,
+    }).renderComponent();
+
+    const components = this.getComponentsData({
+      browserLink,
+    });
+
+    this.data.content = `
+    <div class='h-20 flex justify-between items-center px-5 shadow-md'>
+      <img src="../../assets/images/navbarlogo.svg">
+
+      ${components.content.browserLink}
+    </div>
+    `;
+
+    return this.data;
+  }
+}
+
+export default Navbar;
